203a6d9b35ecefc6e0e108d6ef567df0afe12d57
Fareedah Hussain <fareedah_h@icloud.com>
2026-09-05 19:09:20 +0100

    Add WeekFlow PWA support

---
+++ public/sw.js
@@ -0,0 +1,51 @@
+const CACHE_NAME = 'weekflow-shell-v1'
+const APP_SHELL = ['/', '/index.html', '/favicon.png', '/icon-192.png', '/icon-512.png']
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(APP_SHELL))
+      .then(() => self.skipWaiting()),
+  )
+})
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys()
+      .then((cacheNames) => Promise.all(
+        cacheNames
+          .filter((cacheName) => cacheName !== CACHE_NAME)
+          .map((cacheName) => caches.delete(cacheName)),
+      ))
+      .then(() => self.clients.claim()),
+  )
+})
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET' || new URL(event.request.url).origin !== self.location.origin) return
+
+  if (event.request.mode === 'navigate') {
+    event.respondWith(
+      fetch(event.request)
+        .then((response) => {
+          const responseCopy = response.clone()
+          caches.open(CACHE_NAME).then((cache) => cache.put('/index.html', responseCopy))
+          return response
+        })
+        .catch(() => caches.match('/index.html')),
+    )
+    return
+  }
+
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        if (response.ok) {
+          const responseCopy = response.clone()
+          caches.open(CACHE_NAME).then((cache) => cache.put(event.request, responseCopy))
+        }
+        return response
+      })
+      .catch(() => caches.match(event.request)),
+  )
+})
